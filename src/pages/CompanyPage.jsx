5d26cb9a2680caff5d7b1403e932f0e66d626cd7
genieaitch <hah4815@naver.com>
2025-02-17 12:47:06 +0900

    Initial commit

---
+++ src/pages/CompanyPage.jsx
@@ -0,0 +1,9 @@
+const CompanyPage = ({user}) => {
+    return(
+        <div>
+            <h1>업체 페이지</h1>
+            <p>환영합니다. {user.userName}님</p>
+        </div>
+    )
+}
+export default CompanyPage;
