9357b2e159de7941e6ce0bdf738f0c09329e596f
genieaitch <hah4815@naver.com>
2025-02-17 17:51:48 +0900

    추가

--- src/components/pathRoute.jsx
+++ src/components/pathRoute.jsx
@@ -1,4 +1,6 @@
-// const PathRoute = () => {
+
+
+//const PathRoute = () => {
 // Header 와 Footer 사용
 // 로그인 정보에 따라 보여줄 페이지 설정
 // 이외 전체 페이지 경로 설정
@@ -10,28 +12,37 @@ import Login from "./Login";
 import ProtectedRoute from "./ProtectedRoute";
 import CompanyPage from "../pages/CompanyPage";
 
-function PathRoute() {
-    const [user, setUser] = useState(null);
+function PathRoute () {
+    const[user, setUser] = useState(null);
 
-    return (
-        <BrowserRouter>
+    return(
+        <BrowserRouter >
             {/*
             경로와 관계 없는 jsx 파일은
             Routes 외부에 작성
             */}
+
             <Routes>
                 {/* 0. 관리자, 회사, 유저에 관계 없이 전체 접근 가능 Components */}
-                <Route path="/" element={<Home/>}/>
-                <Route path="/login" element={<Login setUser={setUser}/>}/>
+                <Route path="/" element={<Home/>     } />
+                <Route path="/login" element={<Login setUser={setUser}  />} />
                 {/* 1. 관리자만 접근 가능 Components */}
-                <Route path="/company" element={<ProtectedRoute allowedRoles={[2]}>
-                                                <CompanyPage user={user}/>
-                                                </ProtectedRoute>}/>
-                {/* 2. 회사만 접근 가능 Components */}
-                {/* 3. 유저만 접근 가능 Components */}
+                {/* 2.   회사만 접근 가능 Components */}
+                <Route path="/company" element={   <ProtectedRoute allowedRoles={ [2] }>
+                    <CompanyPage user={user} />
+                </ProtectedRoute>                       }
+                />
+                {/* 3.   유저만 접근 가능 Components */}
+
             </Routes>
+
+
+
+
         </BrowserRouter>
     )
+
+
 }
 
-export default PathRoute;
+export  default PathRoute;
